b6ea2153db20c35f714dca53adc3042a57fc8398
gchristiansen <gchristiansen@velexio.com>
2020-04-28 17:58:41 -0600

    develop (#14)
    release-0.8.0
    
    authored-by: Gerry Christiansen <gc@velexio.com>

---
+++ src/main/java/com/velexio/jlegos/data/annotations/DTOField.java
@@ -0,0 +1,17 @@
+package com.velexio.jlegos.data.annotations;
+
+import com.velexio.jlegos.data.enums.DTOFieldScope;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({FIELD})
+@Retention(RUNTIME)
+@Documented
+public @interface DTOField {
+    DTOFieldScope[] scopes = new DTOFieldScope[]{DTOFieldScope.BASE};
+}
