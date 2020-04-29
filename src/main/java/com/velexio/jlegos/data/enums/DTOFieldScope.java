4d34a4c0f152435bdee0ea4572b895aa7a0a4990
gchristiansen <gchristiansen@velexio.com>
2020-04-28 21:35:33 -0600

    Feature/fix data annotations bug (#15)
    * Fixed issue with DTOField param
    
    release-0.9.0
    
    Co-authored-by: Gerry Christiansen <gc@velexio.com>

--- src/main/java/com/velexio/jlegos/data/enums/DTOFieldScope.java
+++ src/main/java/com/velexio/jlegos/data/enums/DTOFieldScope.java
@@ -1,19 +1,20 @@
 package com.velexio.jlegos.data.enums;
 
 public enum DTOFieldScope {
-    BASE(Names.BASE_NAME),
-    CREATE(Names.CREATE_NAME),
-    UPDATE(Names.UPDATE_NAME),
-    DELETE(Names.DELETE_NAME);
+
+    BASE_DTO(Names.BASE_NAME),
+    CREATE_DTO(Names.CREATE_NAME),
+    UPDATE_DTO(Names.UPDATE_NAME),
+    DELETE_DTO(Names.DELETE_NAME);
 
     DTOFieldScope(String name) {
     }
 
     public static class Names {
-        public static final String BASE_NAME = "BASE";
-        public static final String CREATE_NAME = "CREATE";
-        public static final String UPDATE_NAME = "UPDATE";
-        public static final String DELETE_NAME = "DELETE";
+        public static final String BASE_NAME = "BASE_DTO";
+        public static final String CREATE_NAME = "CREATE_DTO";
+        public static final String UPDATE_NAME = "UPDATE_DTO";
+        public static final String DELETE_NAME = "DELETE_DTO";
     }
 
 }
