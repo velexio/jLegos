055cfa2a34b3598770f1ce0a670d76f6da757078
gchristiansen <gchristiansen@velexio.com>
2023-04-13 00:55:06 -0600

    Fixes for maven plugin to push tags

--- pushCentralMaven.sh
+++ pushCentralMaven.sh
@@ -2,13 +2,11 @@
 
 mvn clean deploy -P release scm:tag
 
-#git push --tags
 git push
 
 echo ""
 echo "**** NOTE *****"
 echo ""
 echo "Remember to: "
-echo "  - push w/ tags"
 echo "  - create PR to merge develop to master"
 echo ""
