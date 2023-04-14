1cf57a66f0c0f5a6413eb7f6baa9b5ad1b9c7b15
gchristiansen <gchristiansen@velexio.com>
2023-04-14 00:16:44 -0600

    Added a bunch of javadoc comments

--- src/main/java/com/velexio/jlegos/exceptions/RemoteHostExecutionException.java
+++
@@ -1,7 +0,0 @@
-package com.velexio.jlegos.exceptions;
-
-public class RemoteHostExecutionException extends Exception {
-    public RemoteHostExecutionException(String message) {
-        super(message);
-    }
-}
