c0c0c3b0f2ffbc36f28c9ad80a595a9b600b54ae
gchristiansen <gchristiansen@velexio.com>
2023-04-17 23:17:28 -0600

    Added more javadocs

--- src/main/java/com/velexio/jlegos/operatingsystem/RemoteHost.java
+++ src/main/java/com/velexio/jlegos/operatingsystem/RemoteHost.java
@@ -4,14 +4,16 @@ import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
-import com.velexio.jlegos.exceptions.CommandExecutionException;
 import com.velexio.jlegos.exceptions.RemoteHostInitializationException;
+import com.velexio.jlegos.util.ExceptionUtils;
 import com.velexio.jlegos.util.StringUtil;
 
 import java.io.ByteArrayOutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Hashtable;
+import java.util.List;
 
 
 /**
@@ -37,7 +39,13 @@ public class RemoteHost {
         this.connectionTimeout = builder.connectionTimeout;
     }
 
-    public CommandResponse execute(String command) throws CommandExecutionException {
+    /**
+     * Allows for executing a command against the remote host
+     *
+     * @param command The command to execute
+     * @return @code CommandResponse object that holds the results of the command
+     */
+    public CommandResponse execute(String command) {
 
         Session session = null;
         ChannelExec channelExec = null;
@@ -72,7 +80,10 @@ public class RemoteHost {
 
 
         } catch (JSchException | InterruptedException e) {
-            throw new CommandExecutionException(e.getLocalizedMessage());
+            return CommandResponse.builder()
+                    .success(false)
+                    .stdErrLines(new ArrayList<>(List.of(ExceptionUtils.getStackTrace(e))))
+                    .build();
         } finally {
             if (session != null) {
                 session.disconnect();
@@ -93,6 +104,11 @@ public class RemoteHost {
         private final Hashtable<String, String> sshConfig;
         private int connectionTimeout;
 
+        /**
+         * Default constructor for builder.
+         *
+         * @param hostname The hostname for the remote connection
+         */
         public Builder(String hostname) {
             this.hostname = hostname;
             this.username = System.getProperty("user.name");
@@ -103,21 +119,46 @@ public class RemoteHost {
             this.sshConfig.put("StrictHostKeyChecking", "no");
         }
 
+        /**
+         * Set the port if different than default (22)
+         *
+         * @param port Port number to use: Default is 22
+         * @return Builder
+         */
         public Builder withPort(int port) {
             this.port = port;
             return this;
         }
 
+        /**
+         * Set the username to be used for remote connection
+         *
+         * @param username The username
+         * @return Builder
+         */
         public Builder withUsername(String username) {
             this.username = username;
             return this;
         }
 
+        /**
+         * Sets the password for user based authentication
+         *
+         * @param password The remote user's password
+         * @return Builder
+         */
         public Builder withPassword(String password) {
             this.password = password;
             return this;
         }
 
+        /**
+         * Adding a private key via file that does not have a passphrase
+         *
+         * @param filePath The full path to the private key file
+         * @return Builder
+         * @throws RemoteHostInitializationException When key is invalid or missing
+         */
         public Builder withPrivateKeyFile(String filePath) throws RemoteHostInitializationException {
             try {
                 sshClient.addIdentity(filePath);
@@ -127,6 +168,14 @@ public class RemoteHost {
             }
         }
 
+        /**
+         * Adding private key via a file path
+         *
+         * @param filePath   The full path to the private key file
+         * @param passphrase The passphrase associated with the key
+         * @return Builder
+         * @throws RemoteHostInitializationException When key is invalid or missing
+         */
         public Builder withPrivateKeyFile(String filePath, String passphrase)
                 throws RemoteHostInitializationException {
             try {
@@ -137,6 +186,14 @@ public class RemoteHost {
             }
         }
 
+        /**
+         * Sets the publice/private keys from String values, uses a default key set name
+         *
+         * @param privateKey String value of the private key
+         * @param publicKey  String value of the public key
+         * @return Builder
+         * @throws RemoteHostInitializationException when keys are invalid
+         */
         public Builder withKeysFromStrings(String privateKey, String publicKey)
                 throws RemoteHostInitializationException {
             try {
@@ -150,6 +207,15 @@ public class RemoteHost {
             }
         }
 
+        /**
+         * Set the keys via String values
+         *
+         * @param keySetName The name of the key pair
+         * @param privateKey String value of the private key
+         * @param publicKey  String value of the public key
+         * @return
+         * @throws RemoteHostInitializationException
+         */
         public Builder withKeysFromStrings(String keySetName, String privateKey, String publicKey)
                 throws RemoteHostInitializationException {
             try {
@@ -163,6 +229,16 @@ public class RemoteHost {
             }
         }
 
+        /**
+         * Allows for setting the keypair with passphrase
+         *
+         * @param keySetName    Name for the key pair
+         * @param privateKey    String value of the private key
+         * @param publicKey     String value of the public key
+         * @param keyPassphrase The string value of passphrase used when key was generated
+         * @return Returns Builder object
+         * @throws RemoteHostInitializationException when unable to initialize because of invalid keys
+         */
         public Builder withKeysFromStrings(String keySetName, String privateKey,
                                            String publicKey, String keyPassphrase)
                 throws RemoteHostInitializationException {
@@ -177,16 +253,32 @@ public class RemoteHost {
             }
         }
 
+        /**
+         * Set the strict host checking option to "yes"
+         *
+         * @return Builder object
+         */
         public Builder withStrictHostCheckingOn() {
             this.sshConfig.put("StrictHostKeyChecking", "yes");
             return this;
         }
 
+        /**
+         * Sets the connection timeout in number of seconds
+         *
+         * @param timeoutSeconds The number of seconds to wait for timeout
+         * @return Builder
+         */
         public Builder withConnectTimeout(int timeoutSeconds) {
             this.connectionTimeout = timeoutSeconds;
             return this;
         }
 
+        /**
+         * Builds the RemoteHost object
+         *
+         * @return RemoteHost
+         */
         public RemoteHost build() {
             return new RemoteHost(this);
         }
